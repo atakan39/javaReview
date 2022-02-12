884e553ff48ace6f841b53c61f04c9a6cd901287
atakantaskin <atakantaskin91@gmail.com>
2022-02-12 00:37:59 -0500

    java review

---
+++ src/javaQuestions/Question61.java
@@ -0,0 +1,31 @@
+package javaQuestions;
+
+public class Question61 {
+    public static void main(String[] args) {
+
+        String word = "aabccdc";
+        char cha = 'c';
+        int count = 0;
+
+        for (int i = 0; i < word.length(); i++) {
+            if (word.charAt(i)==cha) {
+                count++;
+            }
+        }
+        System.out.println(count);
+
+    }
+}
+
+/*
+ @@  61. Write a program that asks user to enter a string and a char, the returns the frequency of the char from the given string
+            Ex:
+                inputs:
+                    str = "aabcccd";
+                    char = 'c';
+
+                output: 3
+
+
+  @ 70. check if two words is anagram.
+ */
---
+++ src/javaQuestions/Questions70.java
@@ -0,0 +1,8 @@
+package javaQuestions;
+
+public class Questions70 {
+    public static void main(String[] args) {
+
+    }
+}
+// @ 70. check if two words is anagram.
